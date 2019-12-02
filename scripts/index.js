9f20180fd877b285811999c799352e2ac6f06ab6
Joe McWhorter <joseph.s.mcwhorter@gmail.com>
2019-12-02 09:45:33 -0500

    Adds basic variable explanations in js

---
+++ scripts/index.js
@@ -0,0 +1,30 @@
+// This is a comment
+// This is another comment
+/*
+What?
+It looks like a css comment
+Yassss.
+
+*/
+
+// Variables! (Storing single pieces of information)
+// turkey_legs_consumed = 3
+var turkeyLegsConsumed = 3;
+// 1. prefix your variables the first time you use them
+// 2. Use camelCase!
+// 3. Terminate each line with ';'
+let turkeysCooked = 2;
+// 'var' was the original way to declare variables.
+// 'let' is better because
+// 1. It gives you more control. (You can limit its use to a specific if/else or a specific loop)
+// 2. 
+
+// 'const' is like 'let' (in that it is local), but:
+// - you must assign it when you declare it.
+// - You can never reassign it.
+
+const numberOfTurkeysICanEat = 1;
+
+// How to use these keywords:
+// 1. Create your variables using 'const'
+// 2. If you find out that you need to reassign it, change it to 'let'.
