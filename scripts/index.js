43987ae46ba6b4c708be5387def6313e87a59734
Joe McWhorter <joseph.s.mcwhorter@gmail.com>
2019-12-02 10:32:12 -0500

    Adds basic notes for methods, properties, and the print function of javascript

--- scripts/index.js
+++ scripts/index.js
@@ -28,3 +28,28 @@ const numberOfTurkeysICanEat = 1;
 // How to use these keywords:
 // 1. Create your variables using 'const'
 // 2. If you find out that you need to reassign it, change it to 'let'.
+
+// print("Hello World!")
+console.log("Hello World!");
+
+// print(turkeysCooked)
+console.log(turkeysCooked);
+
+// print(f"I ate {turkeysCooked} turkeys!")
+console.log(`I ate ${turkeysCooked} turkeys!`);
+// You can use backtick-strings for interpolation.
+// - Template Strings
+// - Template Literals
+
+// print(f"How many turkeys I should eat {numberOfTurkeysICanEat - turkeysCooked}")
+console.log(`Turkeys to eat: ${numberOfTurkeysICanEat - turkeysCooked}`);
+
+const petName = `Sam`;
+// When do I do upper-casing in html vs css vs js?
+// petName.toUpperCase();
+// -wherever you have control
+
+console.log(`${petName.toUpperCase()} is ${petName.length} characters long.`)
+
+// .toUpperCase is a method (i.e., a function that belongs to an object)
+// .length is a property (i.e., a variable that belongs to an object)
